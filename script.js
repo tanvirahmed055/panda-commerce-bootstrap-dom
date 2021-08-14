7e1d732f02009bcb60b42c9f23f86d45c77cfa55
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-08-14 14:02:48 +0600

    v1 to v2

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+//১. নতুন একটা স্ক্রিপ্ট ফাইল খুলবে। সেটাকে index.html এর সাথে কানেক্ট করতে পারো কিনা?
 console.log("connected");
+
+
+
+//২. যতগুলা ইলিমেন্টে h2 ট্যাগ আছে সেগুলাকে সিলেক্ট করে তার লেখাগুলার কালার lightblue করেদিতে পারো কিনা। 
+const secondHeaders = document.getElementsByTagName('h2');
+
+// console.log(secondHeaders);
+for (const secondHeader of secondHeaders) {
+    // console.log(secondHeader);
+    secondHeader.style.color = 'lightblue';
+}
+
+
